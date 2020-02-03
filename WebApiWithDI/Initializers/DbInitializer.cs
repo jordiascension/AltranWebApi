17720df13bc430bb2d295401d0240fcdf50bb027
jordiascension <jordiascension@enterpriseconsulting.es>
2020-02-03 23:32:41 +0100

    Entity Framework With In-Memory Database And Code First.

---
+++ WebApiWithDI/Initializers/DbInitializer.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWithDI.Initializers
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            var context = (TodoContext)serviceProvider.GetService(typeof(TodoContext));
+            
+                var todoItem1 = new Models.TodoItem
+                {
+                    Id = 1,
+                    Name = "Luke",
+                    IsComplete = true
+                };
+
+                context.TodoItems.Add(todoItem1);
+
+                var todoItem2 = new Models.TodoItem
+                {
+                    Id = 2,
+                    Name = "Pepe",
+                    IsComplete = true
+                };
+
+                context.TodoItems.Add(todoItem2);
+
+                context.SaveChanges();
+            
+        }
+    }
+}
---
+++ WebApiWithDI/README.md
@@ -0,0 +1,5 @@
+﻿Install Microsoft.EntityFrameworkCore.SqlServer
+        Microsoft.EntityFrameworkCore.InMemory
+docker exec -it WebApiWithDI bash
+https://localhost:32770/api/TodoItems
+https://localhost:32770/api/TodoItems/1
